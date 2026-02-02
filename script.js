eeee079623a60934446728eb066d025ddfe25a3d
shethisha1312-jpg <shethisha1312@gmail.com>
2026-02-02 20:15:28 +0530

    DOM2 complete

---
+++ script.js
@@ -0,0 +1,54 @@
+// let div=document.querySelector("div");
+
+// console.log(div);
+
+// let id=div.getAttribute("id");
+// console.log(id);
+
+// let name=div.getAttribute("name");
+// console.log(name);
+
+// let para=document.querySelector("p");
+// console.log(para.setAttribute("class","new class"));    
+// let div=document.querySelector("div");
+
+// div.style.backgroundColor ="green";
+// div.style.backgroundColor ="purple";
+// div.style.fontSize  ="20px";            
+
+// div.innerText="Hello11!!";
+// div.style.color="white";
+
+// let newBtn=document.createElement("Button");
+// newBtn.innerText="Click Me!!";
+// console.log(newBtn);
+
+// let div = document.querySelector("div");
+// div.append(newBtn);
+// div.prepend(newBtn);
+// div.before(newBtn);
+// div.after(newBtn);
+
+// let p = document.querySelector("p");
+// p.after(newBtn);
+
+// let newHeading = document.createElement("h1");
+// newHeading.innerHTML="<i> hi , I am new!! </i>";
+
+// document.querySelector("body").prepend(newHeading);
+
+// let para=document.querySelector("p");
+// para.remove();
+
+// //QS-1
+let newBtn=document.createElement("button");
+newBtn.innerText="Click Me!!";
+
+newBtn.style.color="white";
+newBtn.style.backgroundColor="red";
+
+document.querySelector("body").prepend(newBtn);
+
+//QS-2
+
+let para=document.querySelector("p");
